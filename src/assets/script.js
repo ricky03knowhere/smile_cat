6b75dcd3c48fd7a5290ba2378f0758b65bc04288
ricky03knowhere <goose73thecat@gmail.com>
2024-02-29 15:13:41 +0700

    change favicon

--- src/assets/script.js
+++
@@ -1,7 +0,0 @@
-  // <<--- NavLink active handler --->>
-  const navLink = $(".navbar-nav .nav-item .nav-link");
-  navLink.on("click", function () {
-    navLink.removeClass("active");
-
-    $(this).addClass("active");
-  });
